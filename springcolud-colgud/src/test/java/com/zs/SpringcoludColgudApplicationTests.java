b9ac75f860da7fac94f1b91b44eef0f9e02ccf4d
xiaoshuaishuai1997 <477716140@qq.com>
2020-01-15 17:40:01 +0800

    第二次上传

---
+++ springcolud-colgud/src/test/java/com/zs/SpringcoludColgudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcoludColgudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
